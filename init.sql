c29b2279c8bccbe1ae389dc60e7cba5b16d3035a
HJ <staerointern@gmail.com>
2026-09-01 10:20:04 +0800

    update

--- init.sql
+++ init.sql
@@ -20,3 +20,27 @@ CREATE TABLE IF NOT EXISTS report_files (
     debug_path VARCHAR(250),
     FOREIGN KEY (report_id) REFERENCES reports(id)
 );
+
+
+-- Table to store all relevant information for report list display on the app
+
+CREATE TABLE IF NOT EXISTS solar_reports (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    filename VARCHAR(250) NOT NULL,
+    uav_id INT NOT NULL,
+    inspection_datetime DATETIME NOT NULL,
+    status VARCHAR(50) NOT NULL DEFAULT 'Pending'
+);
+
+
+-- Table to store output file paths of every uploaded report
+
+CREATE TABLE IF NOT EXISTS solar_report_files (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    report_id INT NOT NULL,
+    report_path VARCHAR(500) NOT NULL,
+    video_path VARCHAR(250) NOT NULL,
+    debug_path VARCHAR(250),
+    FOREIGN KEY (report_id) REFERENCES reports(id)
+);
+
